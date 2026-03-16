2958936812c896dfcaee6385de8fe17c171f5b85
opvlinkup <146995351+opvlinkup@users.noreply.github.com>
2026-03-16 22:43:45 +0300

    Add initial database migration for `ShortUrls` table with schema and indexes.

---
+++ Diminisher.Infrastructure/Migrations/20260316152138_Init.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ShortUrls",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "binary(16)", nullable: false),
+                    LongUrl = table.Column<string>(type: "varchar(2048)", maxLength: 2048, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LongUrlHash = table.Column<byte[]>(type: "binary(32)", nullable: false),
+                    Code = table.Column<string>(type: "char(10)", maxLength: 10, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    Clicks = table.Column<int>(type: "int", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortUrls", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_Code",
+                table: "ShortUrls",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_CreatedAt",
+                table: "ShortUrls",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_LongUrlHash",
+                table: "ShortUrls",
+                column: "LongUrlHash",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortUrls");
+        }
+    }
+}
